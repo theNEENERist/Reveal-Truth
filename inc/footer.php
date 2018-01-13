39ccf47786614848f6a0cb4dac1dabf979c5dfe0
Joel Leach <joel.b.leach@gmail.com>
2018-01-13 18:50:32 -0500

    Footer changes

--- inc/footer.php
+++ inc/footer.php
@@ -1,59 +1,48 @@
 <footer>
-    <div class="summary">
-        <?php include 'logo.php'?>
-
-        <div class="social">
-            <a href="https://www.facebook.com/revealconferences/?fref=ts" Target="_blank"><img class="facebook" src="img/facebook2.jpg" /></a>
-            <a href="#"><img class="twitter" src="img/twitter.jpg" /></a>
-            <a href="#"><img class="instagram" src="img/instagram.jpg" /></a>
-        </div>
-
-		<div class="sitemapNav">
-			<p class="sitemap">Didn't find what you were looking for?  Try one of these pages.</p>
-
-			<nav>
-				<ul>
-					<li>
-						<a href="/">Home</a>
-					</li>
-					<li>
-						<a href="about.php">About</a>
-						<ul>
-							<li><a href="about.php">About the Conference</a></li>
-							<li><a href="/#location">Location</a></li>
-							<li><a href="commitee.php">Committee</a></li>
-							<li><a href="partners.php">Conference Partners</a></li>
-						</ul>
-					</li>
-					<li>
-						<a href="sessions.php">Sessions</a>
-						<ul>
-							<li><a href="/#speakers">Featured Speakers</a></li>
-							<li><a href="/#schedule">Schedule</a></li>
-						</ul>
-					</li>
-					<li>
-						<a href="contact.php">Register</a>
-						<ul>
-							<li><a href="https://www.eventbrite.com/e/reveal-a-conference-for-your-faith-tickets-41687574658">Tickets</a></li>
-							<li><a href="https://www.paypal.com/donate/?token=UjqaYREJsXgo_ZA04uoGMVwYUZxQwMSTCcjn9sH5OvONNaJBtq32w1WDV7xCHabU4WCmB0">Support the Conference</a></li>
-						</ul>
-					</li>
-					<li><a href="events.php">Events</a></li>
-					<li><a href="contact.php">Contact Us</a></li>
-				</ul>
-			</nav>
-		</div>
+    <div class="summary">		
+		<div class="sponsors">
+			<h3>Partners</h3>
+			<div class="sponsorImg">
+				<a class="sponsor bobrussell" href="http://www.bobrussell.org/"><img src="img/sponsor-bob-russell.png" /></a>
+				<a class="sponsor hikes point" href="http://www.hikespointchristian.com/"><img src="img/sponsor-hikes-point.png" /></a>
+				<a class="sponsor bhpublishing" href="http://www.bhpublishinggroup.com/"><img src="img/sponsor-b-and-h-publishing.png" /></a>
+				<a class="sponsor lyndon" href="http://www.lyndonchristian.org/"><img src="img/sponsor-lyndon.png" /></a>
+				<a class="sponsor kcm" href="http://kentuckianachristianmission.weebly.com/"><img src="img/sponsor-kcm.png" /></a>
+				<a class="sponsor cbs" href="http://cibs-ky.com/"><img src="img/sponsor-cbs.png" /></a>
+				<a class="sponsor clifton heights" href="https://www.facebook.com/CliftonHeightsChristianChurch/?rf=116779421680747"><img src="img/sponsor-clifton-heights.png" /></a>
+			</div>
+		</div>		
     </div>
     
-    <div class="sponsors">
-        <h3>Sponsors</h3>
-        <a class="sponsor bobrussell" href="http://www.bobrussell.org/"><img src="img/sponsor-bob-russell.png" /></a>
-        <a class="sponsor hikes point" href="http://www.hikespointchristian.com/"><img src="img/sponsor-hikes-point.png" /></a>
-<a class="sponsor bhpublishing" href="http://www.bhpublishinggroup.com/"><img src="img/sponsor-b-and-h-publishing.png" /></a>
-<a class="sponsor lyndon" href="http://www.lyndonchristian.org/"><img src="img/sponsor-lyndon.png" /></a><a class="sponsor kcm" href="http://kentuckianachristianmission.weebly.com/"><img src="img/sponsor-kcm.png" /></a>
-<a class="sponsor cbs" href="http://cibs-ky.com/"><img src="img/sponsor-cbs.png" /></a>
-<a class="sponsor clifton heights" href="https://www.facebook.com/CliftonHeightsChristianChurch/?rf=116779421680747"><img src="img/sponsor-clifton-heights.png" /></a>    </div>
-    
-    <p class="copyright">&copy; 2016 Reveal A Conference For Youth Faith.  All Rights Reserved.</p>
+    <div class="footerEnd">
+		<div class="footerEndCenter">
+			<?php include 'logo.php'?>
+			<div class="sitemapNav">
+				<nav>
+					<ul>
+						<div class="navColumnsContainer">
+							<div class="navColumns">
+								<li><a href="about.php">About the Conference</a></li>
+								<li><a href="/#location">Location</a></li>
+								<li><a href="commitee.php">Committee</a></li>
+								<li><a href="partners.php">Conference Partners</a></li>
+							</div>
+							<div class="navColumns">
+								<li><a href="/#speakers">Featured Speakers</a></li>
+								<li><a href="/#schedule">Schedule</a></li>
+								<li><a href="https://www.eventbrite.com/e/reveal-a-conference-for-your-faith-tickets-41687574658">Tickets</a></li>
+							</div>
+							<div class="navColumns">
+								<li><a href="https://www.paypal.com/donate/?token=UjqaYREJsXgo_ZA04uoGMVwYUZxQwMSTCcjn9sH5OvONNaJBtq32w1WDV7xCHabU4WCmB0">Support the Conference</a></li>
+								<li><a href="events.php">Events</a></li>
+								<li><a href="contact.php">Contact Us</a></li>
+							</div>
+						</div>
+					</ul>
+				</nav>
+			</div>
+			
+			<p class="copyright">&copy; 2018 Reveal A Conference For Youth Faith.  All Rights Reserved.</p>
+		</div>
+	</div>
 </footer>
