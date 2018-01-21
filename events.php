4f3165827758b63729d0fc437d66e8f8bb215645
Joel Leach <joel.b.leach@gmail.com>
2018-01-21 17:43:18 -0500

    update events content

--- events.php
+++ events.php
@@ -3,11 +3,93 @@
 
 <section>
     <div class="content">
+        <h3 id="events">Events</h3>
 		<div class="basic">
-			<p>Info coming soon.</p>
-			<p class="text"><a href="index.php">- back -</a></p>
+			<h4>Wednesday, February 7</h4>
+			<ul>
+				<li>
+					<div class="gridTopLeft">
+						<b>I DON'T HAVE ENOUGH FAITH TO BE AN ATHEIST</b>
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						PRESENTED BY FRANK TUREK
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						7:00 PM - 10:00 PM
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						INDIANA UNIVERSITY SOUTHEAST
+					</div>
+					<div class="gridBottom">
+						4201 Grant Line Road, New Albany, IN 47150
+					</div>
+				</li>
+			</ul>
+			<br>
+			<hr />
+			<h4>Jueves, 8 de Febrero</h4>
+			<ul>
+				<li>
+					<div class="gridTopLeft">
+						<b>NO TENGO SUFICIENTE FE PARA SER ATEO</b>
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						PRESENTADO POR JORGE GIL
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						7:00 PM 
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						SANTA MAR&Iacute;A DE LA ANUNCIACI&Oacute;N IGLESIA CAT&Oacute;LICA
+					</div>
+					<div class="gridBottom">
+						415 East 8th Street, New Albany, IN 47150 - la cafeter&iacute;a
+					</div>
+				</li>
+			</ul>
+			<br>
+			<hr />
+			<h4>Saturday, June 30</h4>
+			<ul>
+				<li>
+					<div class="gridTopLeft">
+						<b>2018 REVEAL CONFERENCE</b>
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						FEATURING FRANK TUREK AND ROBERT BOWMAN JR.
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						9:00 AM - 4:30 PM
+					</div>
+				</li>
+				<li>
+					<div class="gridTopLeft">
+						HIKES POINT CHRISTIAN CHURCH
+					</div>
+					<div class="gridBottom">
+						2601 Hikes Lane, Louisville, KY 40218
+					</div>
+				</li>
+			</ul>
+			<br><br>
 		</div>
-	</div>
+    </div>
 </section>
 
 <?php include 'inc/postContent.php'?>
