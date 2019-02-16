57ea1d2a8dda8db8f590596e0e75ff2c12108548
Joel Leach <joel.b.leach@gmail.com>
2019-02-15 21:15:13 -0500

    added favicon and og metat tags

--- inc/preContent.php
+++ inc/preContent.php
@@ -8,6 +8,14 @@
     <script src="js/jquery-2.2.0.min.js"></script>
     <script src="js/main.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=.8">
+    <meta name="description" content="Reveal - A Conference For Your Faith, Louisville, Ky.">
+  	<meta name="theme-color" content="#09243F"/>
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="<?php echo isset($title) ? $title : "Reveal" ?>" />
+	<meta property="og:url" content="<?php echo 'https://www.' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" />
+	<meta property="og:image" content="https://www.reveal-truth.com/img/logo.jpg" />
+	<meta property="og:description" content="The mission of the Reveal Apologetics Conference is to encourage and prepare the average Christian to be able to provide evidence for the truth of Christianity." />
+ 	<link href="/../img/favicon.ico" rel="icon">
     <?php include 'googleAnalytics.php'?>
 </head>
 <body>
